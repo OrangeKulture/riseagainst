7b91743d45fa54addcf000a8bef59d6ad2b61bef
Jorge@OK <ardonz@gmail.com>
2017-04-15 11:52:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  $('.mainButton').click(function() {
+
+    $('.middle-cont').css("visibility", "visible");
+    $('.middle-cont').addClass('animated bounceInDown');
+    $('.mainButton').css("visibility", "hidden");
+  });
+
+  //Slider Options
+  var speed = 500; //fade speed for images
+  var autoslider = true; //autoslider option
+  var autospeed = 10000; //autoslider speed
+
+  $('.slide').first().addClass('active');
+
+  //Hide all slides
+  $('.slide').hide();
+
+  //Show first slide
+  $('.active').show();
+
+  // AutoSlide properties
+  if (autoslider) {
+    setInterval(function() {
+      $('.active').removeClass('active').addClass('lastActive');
+      if ($('.lastActive').is(':last-child')) {
+        $('.slide').first().addClass('active');
+      } else {
+        $('.lastActive').next().addClass('active');
+      }
+      $('.lastActive').removeClass('lastActive');
+      $('.slide').fadeOut(speed);
+      $('.active').fadeIn(speed);
+    }, autospeed);
+  }
+});
